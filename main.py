f847aeea88089acf50a8247e84e49b0f153bd1c8
ss.lukashin <ss.lukashin@tensor.ru>
2024-01-22 19:04:46 +0700

    debug

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import config
-from person import Person
-from contract import Contract
-from creative import Creative
-from pad import Pad
-from statistics import Statistics
-
+from tabs.person import Person
+from tabs.contract import Contract
+from tabs.creative import Creative
+from tabs.pad import Pad
+from tabs.statistics import Statistics
 
 if __name__ == '__main__':
-
     print(f"Включен режим - {'PROD' if config.PROD else 'DEV'}")
 
-    person = Person()
-    person_list = person.get_list()
-    print(person_list)
-
+    # person = Person()
+    # person_list = person.get_list()
+    # print(person_list)
+    #
     contract = Contract()
     contract_list = contract.get_list()
     print(contract_list)
 
     creative = Creative()
     creative_list = creative.get_list()
-    print(creative_list)
+    cr_1 = {}
+    cr_response = creative.add_creative(contract_name="", file_name="", **cr_1)
+    print(cr_response['erid'])
 
-    pad = Pad()
-    pad_list = pad.get_list()
-    print(pad_list)
-
-    statistics = Statistics()
-    statistics_list = statistics.get_list()
-    print(statistics_list)
+    # pad = Pad()
+    # pad_list = pad.get_list()
+    # print(pad_list)
+    #
+    # statistics = Statistics()
+    # statistics_list = statistics.get_list()
+    # print(statistics_list)
 
     print("Все!")
